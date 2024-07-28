89e5d6fb6b67ea1f65628ef33ab6efdecec1e158
Pritam12F <pritam.das.santu@gmail.com>
2024-07-28 17:21:34 +0000

    added basic landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import "./globals.css"
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold underline">
-      Hi there
-    </div>
+    <main className="flex items-center justify-center w-screen h-screen bg-fade">
+    <span className="flex flex-col text-center w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl px-4 sm:px-6 md:px-12 lg:px-20 py-12 md:py-16 lg:py-24">
+      <div className="my-3 text-xl sm:text-2xl md:text-3xl font-semibold text-blue-700">
+        Serene
+      </div>
+      <div className="my-3 text-sm sm:text-base md:text-lg lg:text-xl text-slate-500 typing-animation">
+        Boost your productivity.
+      </div>
+    </span>
+  </main>
   );
 }

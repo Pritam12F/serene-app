80583d7f859abedaa923cd4a78d37174b2dda189
Pritam12F <pritam.das.santu@gmail.com>
2024-08-02 12:27:11 +0000

    added shadcn

--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./ui/button";
+
 export const Appbar = () => {
   return (
-    <div className="flex bg-red-700">
-      <button>Sign in</button>
+    <div className="flex justify-between px-5 bg-customPurple py-3">
+      <div className="text-slate-200">Hamburger menu</div>
+      <div className="space-x-5">
+        <Button variant={"secondary"}>Join now</Button>
+        <Button variant={"secondary"}>Sign in</Button>
+      </div>
     </div>
   );
 };
